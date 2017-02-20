06c040639e1e519df616d281615904198b86cb59
Yanhong Wu <wdan1990@gmail.com>
2017-02-20 18:05:37 +0800

    minor fix

--- client/src/services/pipe-service.js
+++ client/src/services/pipe-service.js
@@ -2,10 +2,7 @@ import Vue from 'vue';
 
 const PipeService = new Vue({
     data: {
-        DATA_CHANGE: 'data-change',
-        SUBATTRIBUTE_UPDATE: 'subattribute-update',
-        SELECT_POINT: 'select-point',
-        SWITCH_TO_FULL_DATA: 'full-data',
+        'DATA_READY': 'data-ready',
     },
 });
 
